5749a5b4b978279ae7431a3f804092e02a590bea
Amanda Hult <amandis1@live.se>
2023-01-30 22:12:35 +0100

    left and right click fixes

--- assignment2/main.py
+++ assignment2/main.py
@@ -1,5 +1,6 @@
-import pandas as pd
+# import pandas as pd
 import tkinter as tk
+import numpy as np
 import math
 
 COLORS = ["red", "green", "blue"]
@@ -18,9 +19,11 @@ class ScatterPlot:
     def __init__(self, data):
         self.data = data
         self.translated = False
+        self.highlighted = False
+        self.latest_move = [0,0]
+        self.closest_points = []
 
-
-        
+   
     def get_type(self, point):
         return point.type
 
@@ -34,11 +37,11 @@ class ScatterPlot:
             # point.y = round(400 - i*(350/x_range))  
             index = types.index(point.type)
             if index == 0:    
-                element = self.canvas.create_oval(point.x-5, point.y-5, point.x+5, point.y+5, fill=COLORS[index], tags=["point", f"shape{i}"])
+                element = self.canvas.create_oval(point.x-5, point.y-5, point.x+5, point.y+5, fill=COLORS[index], tags=["point", f"shape{i}", f"{COLORS[index]}"])
             elif index == 1:
-                element = self.canvas.create_rectangle(point.x-5, point.y-5, point.x+5, point.y+5, fill=COLORS[index], tags=["point", f"shape{i}"])                
+                element = self.canvas.create_rectangle(point.x-5, point.y-5, point.x+5, point.y+5, fill=COLORS[index], tags=["point", f"shape{i}", f"{COLORS[index]}"])                
             else:
-                element = self.canvas.create_text(point.x, point.y, text="+", fill=COLORS[index], font=("Purisa", 30), tags=["point", f"shape{i}"])
+                element = self.canvas.create_text(point.x, point.y, text="+", fill=COLORS[index], font=("Purisa", 30), tags=["point", f"shape{i}", f"{COLORS[index]}"])
                      
             self.canvas.tag_bind(element, '<Button-1>', self.object_left_click_event) # Left click
             self.canvas.tag_bind(element, '<Button-3>', self.object_right_click_event) # Right click 
@@ -126,41 +129,87 @@ class ScatterPlot:
 
 
     def object_left_click_event(self, event):
-        # self.canvas.itemconfigure(event.num, fill="blue")
-        # print('Clicked object at: ', self.canvas.coords(event.num), event.num)    
-        # Move all with tag "shape"
-        
-        move_x = 400-event.x
-        move_y = 400-event.y
-        
-        point_ids = self.canvas.find_withtag("point")
 
-        # Loop through all points with tag "point"
-        for i in point_ids:
-            self.canvas.move(i, move_x, move_y)
+        points = self.canvas.find_withtag("point")
+   
+        # Different actions depending on already translated
+        if self.translated:
+            self.translated = False
+
+            # Move all points back to original
+            for i in points:
+                self.canvas.move(i, -self.latest_move[0], -self.latest_move[1])
+                # Switch color back to ordinary
+                colorTag = self.canvas.gettags(i)[2]
+                self.canvas.itemconfig(i, fill=colorTag)
+            
+        else:
+            self.translated = True
+            # Move points
+            move_x = 400-event.x
+            move_y = 400-event.y
+            self.latest_move = [move_x, move_y]           
+                    
+            # Loop through all points with tag "point"
+            for i in range(len(points)):
+                self.canvas.move(points[i], move_x, move_y)
+                tag = self.canvas.gettags(points[i])[1]
+                
+                # Extract index from element
+                index = ""
+                for s in tag:
+                    if s.isnumeric():
+                        index += s
+                
+                p = self.canvas.coords(points[i])
+             
+                # Change color
+                if p[0] > 400 and p[1] > 400:
+                    self.canvas.itemconfig(points[i], fill="red")
+                elif p[0] > 400 and p[1] < 400:
+                    self.canvas.itemconfig(points[i], fill="blue")                
+                elif p[0] < 400 and p[1] < 400:
+                    self.canvas.itemconfig(points[i], fill="black")
+                elif p[0] < 400 and p[1] > 400:
+                    self.canvas.itemconfig(points[i], fill="orange")
+
+            current = event.widget.find_withtag("current")[0]
+            self.canvas.itemconfig(current, fill='yellow')
+                
 
     def object_right_click_event(self, event):
-        # Get tags of current element
-        current = event.widget.find_withtag("current")[0]
-        tag = self.canvas.gettags(current)[1]
+        if self.highlighted: 
+            self.highlighted = False
+            for p in self.closest_points:
+                colorTag = self.canvas.gettags(p)[2]
+                self.canvas.itemconfig(p, fill=colorTag)
+        else:
+            self.highlighted = True
+            points = self.canvas.find_withtag("point")
 
-        # Extract index from element
-        index = ""
-        for s in tag:
-            if s.isnumeric():
-                index += s
+            # Get tags of current element
+            current = event.widget.find_withtag("current")[0]
         
-        
-        active = self.data[int(index)]
+            active = self.canvas.coords(current)            
 
-        dist = []
-        for i in range(len(data)):
-            dist.append(math.sqrt(pow((active.x-data[i].x),2)+pow((active.y-data[i].y),2)))
+            dist = []   
+            p = []
+            for point in points:
+                co = self.canvas.coords(point)
+                d = math.sqrt(pow((active[0]-co[0]),2)+pow((active[1]-co[1]),2))  
+                            
+                if d > 0:                 
+                    dist.append(d)
+                    # p.append(point)
+            closest_points_indices = np.argpartition(dist, 5)[:5] 
+                
+            # Get 5 smallest distances               
+            self.closest_points = []
 
-        for i in range(0,5):
-            print(i)
-
- 
+            for i in closest_points_indices:            
+                self.canvas.itemconfig(points[i], fill='yellow')                    
+                self.closest_points.append(points[i])            
+                
        
 # load csv
 def load_csv(file):
@@ -176,7 +225,7 @@ def load_csv(file):
     return data
 
 if __name__ == "__main__":
-    data = load_csv("data2.csv")
+    data = load_csv("data1.csv")
     # scatter_plot(data)
     sp = ScatterPlot(data)
     sp.scatter_plot()
