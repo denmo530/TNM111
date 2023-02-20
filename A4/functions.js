bb4517fb45c663edfd26c20f532653e7c7d2bd75
Amanda Hult <amandis1@live.se>
2023-02-20 21:55:37 +0100

    lab4 done

--- A4/functions.js
+++ A4/functions.js
@@ -1,20 +1,155 @@
-function drawGraph(data) {
+/**
+ * MAIN CHARACTER FUNCTIONS
+ */
+function getMainCharacters(data) {
+  const mainChars = data.nodes.filter((char) => char.colour !== "#808080");
+
+  return mainChars;
+}
+
+/**
+ * Function to handle click on character
+ * @param {*} event
+ */
+function characterClick(event, link, node) {
+  let checkedBoxes = document.querySelectorAll("input[type=checkbox]:checked");
+
+  let checkedNames = Array.prototype.slice
+    .call(checkedBoxes)
+    .map((box) => box.name);
+
+  if (checkedNames.length > 0) {
+    node.transition().attr("opacity", 0.3);
+    link.attr("opacity", 0.1);
+
+    checkedNames.map((char) => {
+      d3.selectAll(".node-" + char.replace(/ /g, ""))
+        .transition()
+        .attr("opacity", 1.0);
+
+      let d = d3.select(".node-" + char.replace(/ /g, "")).data()[0];
+
+      link.data().forEach((item) => {
+        if (item.source.index === d.index || item.target.index === d.index) {
+          link
+            .filter((l) => l.index == item.index)
+            .transition()
+            .attr("opacity", 1)
+            .attr("stroke-width", 2);
+        }
+      });
+    });
+  } else {
+    // No names selected
+    node.transition().attr("opacity", 1);
+    link.transition().attr("opacity", 1).attr("stroke-width", 2);
+  }
+}
+/**
+ * Function to filter and combine data from multiple episodes
+ * @returns
+ */
+function filterEpisodeData(originData) {
+  let checkedBoxes = document.querySelectorAll(
+    "#dropdown-list2 input[type=checkbox]:checked"
+  );
+  let filteredData = [];
+
+  // Add data from episode that corresponds to checkboxes
+  if (checkedBoxes.length > 0) {
+    checkedBoxes.forEach((box) => filteredData.push(allData[box.value - 1]));
+  } else {
+    return originData;
+  }
+
+  // Extract nodes and links from data
+  let nodes = filteredData.map((item) => item.nodes);
+  let links = filteredData.map((item) => item.links);
+
+  let uniqueNodes = [];
+  let uniqueLinks = [];
+
+  // Loop through all episodes in nodes
+  for (let index = 0; index < nodes.length; index++) {
+    let episodeNodes = nodes[index];
+    let episodeLinks = links[index];
+
+    // Loop through all nodes in current episodes
+    episodeNodes.forEach((currNode, i) => {
+      // Check if name already exists in uniqueNodes
+      if (uniqueNodes.some((node) => node.name === currNode.name)) {
+        let uniqueNodesIndex = uniqueNodes.findIndex(
+          (node) => node.name === currNode.name
+        );
+
+        // Increase value of existing node
+        uniqueNodes[uniqueNodesIndex].value += currNode.value;
+
+        // Update source and target on all connected links to current node
+        episodeLinks.map((link) => {
+          if (link.target === i) {
+            return { ...link, target: uniqueNodesIndex };
+          } else if (link.source === i) {
+            return { ...link, source: uniqueNodesIndex };
+          }
+          return link;
+        });
+      } else {
+        uniqueNodes.push(currNode);
+      }
+    });
+
+    episodeLinks.forEach((link) => {
+      const containsLink = uniqueLinks.some(
+        (obj) => obj.target === link.target && obj.source === link.source
+      );
+      if (containsLink) {
+        // Add values on same links
+        let index = uniqueLinks.findIndex(
+          (obj) => obj.target === link.target && obj.source === link.source
+        );
+        uniqueLinks[index].value += link.value;
+      } else {
+        // Link does not exists
+        uniqueLinks.push(link);
+      }
+    });
+  }
+
+  let combinedData = { nodes: uniqueNodes, links: uniqueLinks };
+  console.log(combinedData);
+  return combinedData;
+}
+
+/**
+ * Main function to draw graph
+ * @param {*} data
+ * @param {*} type
+ * @returns
+ */
+function drawGraph(data, type, graphIndex) {
+  let originData = data;
+  let checkedBoxes = document.querySelectorAll(
+    "#dropdown-list2 input[type=checkbox]:checked"
+  );
+
+  if (type === "episodes" && checkedBoxes.length > 0) {
+    data = filterEpisodeData(originData);
+  }
+
   // Set size of window for graph and margins
   let margin = { top: 10, right: 30, bottom: 30, left: 40 };
   let width = 1000 - margin.left - margin.right;
-  let height = 1000 - margin.top - margin.bottom;
-
-  let dropdownList = document.getElementById("dropdown-list1");
+  let height = 1300 - margin.top - margin.bottom;
 
   let svg = d3
     .select("#graph")
     .append("svg")
+    .attr("id", `graph-${graphIndex}`)
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  console.log(data);
 
   let link = svg
     .append("g")
@@ -46,8 +181,9 @@ function drawGraph(data) {
     .enter()
     .append("g")
     .append("circle")
-    .attr("r", 30)
+    .attr("r", 15)
     .attr("fill", (d) => d.colour)
+    .attr("class", (d) => "node-" + d.name.replace(/ /g, ""))
     .classed("fixed", (d) => d.fx !== undefined)
     .classed("nodes", true)
     .call(
@@ -56,35 +192,13 @@ function drawGraph(data) {
     );
 
   /**
-   * LABELS
+   * HOVER FUNCTIONS
    */
-  let label = svg
-    .append("g")
-    .attr("class", "labels")
-    .selectAll("text")
-    .data(data.nodes)
-    .enter()
-    .append("text")
-    .text(function (d) {
-      return d.name;
-    })
-    .attr("class", "label");
-  label
-    .style("text-anchor", "middle")
-    .style("font-size", 12)
-    .attr("fill", "white");
-
-  node.append("title").text((d) => `${d.name}\nValue: ${d.value}`);
-  node.attr("id", (d) => "node-" + d.name);
-  node
-    .append("text")
-    .attr("x", (d) => d.cx)
-    .attr("y", (d) => d.cy)
-    .text((d) => d.name);
 
   link.on("mouseover", function (d) {
     link.attr("opacity", 0.2);
     d3.select(this).attr("opacity", 1);
+
     // linkLabel.text("Link from " + d.source.name + " to " + d.target.name);
     let text = `Link from ${d.source.name} to ${d.target.name} <br> Number of interactions: ${d.value}`;
     linkLabel.html(text);
@@ -108,6 +222,7 @@ function drawGraph(data) {
   let toolTip = d3
     .select("body")
     .append("div")
+    .attr("id", "toolTip")
     .style("position", "absolute")
     .style("color", "white")
     .style("padding", "8px")
@@ -118,67 +233,6 @@ function drawGraph(data) {
     .style("height", "auto")
     .style("visibility", "hidden")
     .text("");
-
-  // Mouse events for tooltips
-  label.on("mouseover", function (d) {
-    let text = `Name: ${d.name} <br> Number of scenes: ${d.value}`;
-    toolTip.html(text);
-
-    toolTip.style("visibility", "visible");
-  });
-  label.on("mousemove", function () {
-    toolTip
-      .style("top", d3.event.pageY - 10 + "px")
-      .style("left", d3.event.pageX - 10 + "px");
-  });
-
-  console.log(toolTip);
-
-  /**
-   * HOVER STYLE
-   */
-
-  // Add mouseover on the nodes
-  node.on("mouseover", function (d) {
-    // toolTip.html(`${d.name}`);
-    // toolTip.style("visibility", "visible");
-
-    // Lower opacity on all nodes
-    node.transition().attr("opacity", 0.3);
-    link.attr("opacity", 0.1);
-    // Label opacity lowered
-    label.transition().attr("opacity", 0.3);
-
-    // Style for active node
-    d3.select(this).transition().attr("opacity", 1);
-
-    // Set opacity to 1 on connected links
-    link.data().forEach((item) => {
-      if (item.source.index === d.index || item.target.index === d.index) {
-        link
-          .filter((l) => l.index == item.index)
-          .transition()
-          .attr("opacity", 1)
-          .attr("stroke-width", 2);
-      }
-    });
-  });
-
-  node.on("mousemove", function (d) {
-    toolTip
-      .style("top", d3.event.pageY - 10 + "px")
-      .style("left", d3.event.pageX - 10 + "px");
-  });
-
-  node.on("mouseout", function (d) {
-    // Reset style
-    d3.select(this).attr("fill", (n) => n.colour);
-    node.transition().attr("opacity", 1);
-    link.attr("opacity", 1);
-    label.attr("opacity", 1);
-
-    toolTip.style("visibility", "hidden");
-  });
 
   /**
    * SIMULATION
@@ -191,7 +245,7 @@ function drawGraph(data) {
       d3.forceLink().id((d) => d.index)
     )
     .force("center", d3.forceCenter(width / 2, height / 3))
-    .force("collission", d3.forceCollide().radius(50))
+    .force("collission", d3.forceCollide().radius(35))
     .force("charge", d3.forceManyBody().strength(-20));
 
   simulation.nodes(data.nodes).on("tick", () => {
@@ -202,31 +256,10 @@ function drawGraph(data) {
       .attr("y2", (d) => d.target.y);
 
     node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
-    label.attr("x", (d) => d.x).attr("y", (d) => d.y);
+    // label.attr("x", (d) => d.x).attr("y", (d) => d.y);
   });
 
   simulation.force("link").links(data.links);
-
-  let mainCharacters = getMainCharacters(data);
-
-  mainCharacters.forEach((character) => {
-    let label = document.createElement("label");
-    let input = document.createElement("input");
-
-    label.text = character.name;
-    input.type = "checkbox";
-    input.name = character.name;
-    input.value = character.name;
-    input.onclick = function (event) {
-      characterClick(event.target.name);
-    };
-
-    label.innerHTML = character.name;
-    label.htmlFor = character.name;
-
-    label.appendChild(input);
-    dropdownList.appendChild(label);
-  });
 
   /**
    * NODE DRAG FUNCTIONS
@@ -243,49 +276,5 @@ function drawGraph(data) {
     d.fy = d3.event.y;
   }
 
-  /**
-   * MAIN CHARACTER FUNCTIONS
-   */
-  function getMainCharacters() {
-    const mainChars = data.nodes.filter((char) => char.colour !== "#808080");
-    //   let positions = mainChars.map((char) => {
-    //   });
-    return mainChars;
-  }
-
-  function characterClick(event) {
-    let checkedBoxes = document.querySelectorAll(
-      "input[type=checkbox]:checked"
-    );
-
-    let checkedNames = Array.prototype.slice
-      .call(checkedBoxes)
-      .map((box) => box.name);
-
-    if (checkedNames.length > 0) {
-      node.transition().attr("opacity", 0.3);
-      link.attr("opacity", 0.1);
-
-      checkedNames.map((char) => {
-        d3.select("#node-" + char)
-          .transition()
-          .attr("opacity", 1);
-        let d = d3.select("#node-" + char).datum();
-
-        link.data().forEach((item) => {
-          if (item.source.index === d.index || item.target.index === d.index) {
-            link
-              .filter((l) => l.index == item.index)
-              .transition()
-              .attr("opacity", 1)
-              .attr("stroke-width", 2);
-          }
-        });
-      });
-    } else {
-      // No names selected
-      node.transition().attr("opacity", 1);
-      link.transition().attr("opacity", 1).attr("stroke-width", 2);
-    }
-  }
+  return { node, link, toolTip, svg };
 }
--- A4/index.js
+++ A4/index.js
@@ -9,75 +9,195 @@ function combineEpisodes() {
       }
     );
   }
+
   return allData;
 }
 
-function createEpisodeSelect() {
-  let dropdownList = document.getElementById("dropdown-list2");
-
-  let episodes = [1, 2, 3, 4, 5, 6, 7];
-  console.log(episodes);
-  episodes.forEach((episode) => {
-    let label = document.createElement("label");
-    let input = document.createElement("input");
-
-    input.type = "checkbox";
-    input.name = episode.toString();
-    input.class = "episode-checkbox";
-    input.value = episode;
-    input.onclick = function (event) {
-      filterEpisodeData(event.target.name);
-    };
-
-    label.innerHTML = episode.toString();
-    label.htmlFor = episode.toString();
-
-    label.appendChild(input);
-    dropdownList.appendChild(label);
-  });
-}
-
-function filterEpisodeData(event) {
-  let checkedBoxes = document.querySelectorAll(
-    "#dropdown-list2 input[type=checkbox]:checked"
-  );
-  let filteredData = [];
-
-  // Add data from episode that corresponds to checkboxes
-  checkedBoxes.forEach((box) => filteredData.push(allData[box.value - 1]));
-
-  let nodes = filteredData.map((item) => item.nodes);
-  let links = filteredData.map((item) => item.links);
-
-  let uniqueNodes = [];
-  nodes.flat().reduce((arr, curr) => {
-    if (uniqueNodes.some((node) => node.name === curr.name)) {
-      uniqueNodes.find((node) => node.name === curr.name).value += curr.value;
-    } else {
-      uniqueNodes.push(curr);
-    }
-  }, {});
-
-  let combinedData = { nodes: uniqueNodes, links: links.flat() };
-  console.log(combinedData);
-
-  return combinedData;
-}
-
-// drawGraph(data);
-
-createEpisodeSelect();
-
 let allData = combineEpisodes();
-
-// drawGraph(allData);
 
 console.log(allData);
 
 d3.json(
   "./starwars-interactions/starwars-full-interactions-allCharacters.json",
   (data) => {
-    console.log("test");
-    drawGraph(data);
+    let graph1 = drawGraph(data, "all", 1);
+    let graph2 = drawGraph(data, "episodes", 2);
+
+    graph1.node.on("mouseover", highlightNode);
+    graph2.node.on("mouseover", highlightNode);
+
+    graph1.node.on("mouseout", resetNode);
+    graph2.node.on("mouseout", resetNode);
+
+    characterMenu(data);
+
+    function resetNode() {
+      // Reset style
+      d3.select(this).attr("fill", (n) => n.colour);
+
+      // label.attr("opacity", 1);
+
+      graph1.node.transition().attr("opacity", 1.0);
+      graph2.node.transition().attr("opacity", 1.0);
+
+      graph1.link.transition().attr("opacity", 1.0);
+      graph2.link.transition().attr("opacity", 1.0);
+
+      d3.select("#toolTip").style("visibility", "hidden");
+    }
+
+    createEpisodeSelect();
+
+    function createEpisodeSelect() {
+      let dropdownList = document.getElementById("dropdown-list2");
+
+      let episodes = [1, 2, 3, 4, 5, 6, 7];
+
+      episodes.forEach((episode) => {
+        let label = document.createElement("label");
+        let input = document.createElement("input");
+
+        input.type = "checkbox";
+        input.name = episode.toString();
+        input.class = "episode-checkbox";
+        input.value = episode;
+
+        input.onclick = function (event) {
+          let data = filterEpisodeData(event.target.value);
+
+          // graph2.node.remove();
+          // graph2.link.remove();
+          // graph2.select("#graph").remove();
+          d3.select("#graph-2").remove();
+
+          graph2 = drawGraph(data, "episode", 2);
+          graph2.link.data(data.links);
+
+          graph2.node
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) {
+              return d.x;
+            })
+            .attr("cy", function (d) {
+              return d.y;
+            })
+            .attr("r", function (d) {
+              return d.r;
+            });
+
+          graph2.node.exit().remove();
+
+          graph2.node.on("mouseover", highlightNode);
+          graph2.node.on("mouseout", resetNode);
+        };
+
+        label.innerHTML = episode.toString();
+        label.htmlFor = episode.toString();
+
+        label.appendChild(input);
+        dropdownList.appendChild(label);
+      });
+    }
+
+    // /**
+    //  * DRAG FUNCTIONS
+    //  * @param {*} d
+    //  */
+    // function dragStarted(d) {
+    //   if (!d3.event.active) simulation.alphaTarget(0.03).restart();
+    //   d.fx = d.x;
+    //   d.fy = d.y;
+    // }
+
+    // function dragged(d) {
+    //   // Check
+    //   d.fx = d3.event.x;
+    //   d.fy = d3.event.y;
+    // }
+
+    function highlightNode() {
+      let text = `Name: ${
+        d3.select(this).data()[0].name
+      } <br> Number of scenes: ${d3.select(this).data()[0].value}`;
+      graph1.toolTip.html(text);
+
+      graph1.toolTip.style("visibility", "visible");
+
+      graph1.toolTip.style("top", 0).style("left", 0);
+
+      let id = d3.select(this).attr("class");
+
+      graph1.node.transition().attr("opacity", 0.3);
+      graph2.node.transition().attr("opacity", 0.3);
+
+      graph1.link.transition().attr("opacity", 0.1);
+      graph2.link.transition().attr("opacity", 0.1);
+
+      // Set opacity to 1 on hovered nodes
+
+      id = id.split(" ")[0];
+      console.log(id);
+      d3.selectAll(`.${id}`).transition().attr("opacity", 1);
+
+      graph1.link.data().forEach((item) => {
+        console.log(item);
+        console.log(d3.selectAll(`.${id}`));
+
+        if (
+          item.source.index === d3.select(`.${id}`).data()[0].index ||
+          item.target.index === d3.select(`.${id}`).data()[0].index
+        ) {
+          graph1.link
+            .filter((l) => l.index == item.index)
+            .transition()
+            .attr("opacity", 1)
+            .attr("stroke-width", 2);
+        }
+      });
+
+      graph2.link.data().forEach((item) => {
+        if (
+          item.source.index === d3.select(`.${id}`).data()[0].index ||
+          item.target.index === d3.select(`.${id}`).data()[0].index
+        ) {
+          graph2.link
+            .filter((l) => l.index == item.index)
+            .transition()
+            .attr("opacity", 1)
+            .attr("stroke-width", 2);
+        }
+      });
+    }
+
+    /**
+     * Function to render character menu
+     * @param {*} data
+     */
+    function characterMenu(data, link, node) {
+      let mainCharacters = getMainCharacters(data);
+      let dropdownList = document.getElementById("dropdown-list1");
+      mainCharacters = new Set(mainCharacters);
+
+      mainCharacters.forEach((character) => {
+        let label = document.createElement("label");
+        let input = document.createElement("input");
+
+        label.text = character.name;
+        input.type = "checkbox";
+        input.name = character.name;
+        input.value = character.name;
+        input.onclick = function (event) {
+          characterClick(event.target.name, graph1.link, graph1.node);
+          characterClick(event.target.name, graph2.link, graph2.node);
+        };
+
+        label.innerHTML = character.name;
+        label.htmlFor = character.name;
+
+        label.appendChild(input);
+        dropdownList.appendChild(label);
+      });
+    }
   }
 );
